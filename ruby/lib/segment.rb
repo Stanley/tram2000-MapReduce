9040d72ffa212a31f6273e0d2beb85b4c4e73307
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2010-06-02 15:52:31 +0200

    fixed intersection calculations

--- ruby/lib/segment.rb
+++ ruby/lib/segment.rb
@@ -2,7 +2,7 @@
 class Segment
 
   R = 6371 * 1000   # Earth radius in meters
-  Rad = 3.142 / 180 # Converts degrees do radians
+  Rad = Math::PI / 180 # Converts degrees do radians
 
   # Haversine formula
   def self.length(a, b)
@@ -28,8 +28,8 @@ class Segment
     Segment.length(@a, @b)
   end
 
-  # Finds segment intersecion(s) with circle (ellipse)
-  # In case of two points, the one closer to segment will be returned
+  # Finds segment intersecion(s) with circle
+  # In case of two points, the one closer to segment beginning (@a) will be returned
   # Input: circle center and radius
   # Output: null or [lat, lng] of intersecion
   def intersection(lat, lng, r)
@@ -37,66 +37,47 @@ class Segment
     # Convert degrees to meters
     x1 = Segment.length([lat, lng], [lat, @a[1]]) # lng
     x1 *= -1 if lng > @a[1]
-#p "Segment.length([#{lat}, #{lng}], [#{lat}, #{@a[1]}]) = #{x1}"
 
     y1 = Segment.length([lat, lng], [@a[0], lng]) # lat
     y1 *= -1 if lat > @a[0]
-#p "Segment.length([#{lat}, #{lng}], [#{@a[0]}, #{lng}]) = #{y1}"
         
     x2 = Segment.length([lat, lng], [lat, @b[1]]) # lng
     x2 *= -1 if lng > @b[1]
-#p "Segment.length([#{lat}, #{lng}], [#{lat}, #{@b[1]}]) = #{x2}"
 
     y2 = Segment.length([lat, lng], [@b[0], lng]) # lat
     y2 *= -1 if lat > @b[0]
-#p "Segment.length([#{lat}, #{lng}], [#{@b[0]}, #{lng}]) = #{y2}"
     
-    # ???
     # Circle equation: lat**2 + lng**2 = r**2
-    # Segment equation: y = y1 + (x-x1)/(x2-x1) * (y2-y1)
-    sol = []
-    sol.push (-Math.sqrt((-2*x1*y1**2+2*x1*y1*y2+2*x1+2*x2*y1**2-2*x2*y1*y2)**2-4*(-x1**2+2*x1*x2-x2**2-1)*(r**2*x1**2-2*r**2*x1*x2+r**2*x2**2+x1**2*y1**2-2*x1**2*y1*y2-x1**2+2*x1*x2*y1*y2-x2**2*y1**2))+2*x1*y1**2-2*x1*y1*y2-2*x1-2*x2*y1**2+2*x2*y1*y2)/(2*(-x1**2+2*x1*x2-x2**2-1))
+    # Segment equation: lat = y1 + (lng-x1)/(x2-x1) * (y2-y1)
+    # See also: http://mathworld.wolfram.com/Circle-LineIntersection.html
+    
+    dx = x2 - x1
+    dy = y2 - y1
+    dr = Math.sqrt(dx**2 + dy**2)
+    d = x1*y2 - x2*y1    
+    delta = r**2 * dr**2 - d**2    
 
-    sol.push ( Math.sqrt((-2*x1*y1**2+2*x1*y1*y2+2*x1+2*x2*y1**2-2*x2*y1*y2)**2-4*(-x1**2+2*x1*x2-x2**2-1)*(r**2*x1**2-2*r**2*x1*x2+r**2*x2**2+x1**2*y1**2-2*x1**2*y1*y2-x1**2+2*x1*x2*y1*y2-x2**2*y1**2))+2*x1*y1**2-2*x1*y1*y2-2*x1-2*x2*y1**2+2*x2*y1*y2)/(2*(-x1**2+2*x1*x2-x2**2-1))
+    sgn = lambda{ |x| x < 0 ? -1 : 1 }
     
-    # Segment equation
-    f = lambda{ |x| y1 + (x-x1)/(x2-x1) * (y2-y1) if (x1..x2).include?(x) || (x2..x1).include?(x) }
-    
-#    # Solve the simultaneous equations (segment + circle)
-#    # y**2 = y1**2 + (x-x1)**2 / (x2-x1)**2 * (y2-y1)**2 + 2*y1 * (x-x1)/(x2-x1) * (x2-y1)
-#    _t = (y2-y1)**2 / (x2-x1)**2
-#    _u = (y2-y1) / (x2-x1)
-#    
-#    a = 1 + _t
-#    b = -2*x1*_t + 2*y1*_u
-#    c = y1**2 + x1**2*_t + 2*y1*(-x1/x2-x1)*_u - r**2
-#    
-#    # Delta
-#    delta = b**2 - 4*a*c
-    
-    if true #delta > 0
+    if delta > 0
           
-      #d = Math.sqrt delta
-      # Results
-#      [(-b-d) / 2*a,  (-b+d) / 2*a].each do |x|
+      # Return closest point (to point @a) of two
+      [-1, 1].map do |sign|
+      
+        x = (d*dy + sign * sgn[dy] * dx * Math.sqrt(delta)) / dr**2
+        y = (-d*dx + sign * dy.abs * Math.sqrt(delta)) / dr**2
 
-      sol.each do |x|
-
-        y = f[x] || next
-
-        # What we're really looking for
         intersection_lat = 180*y / (Math::PI * R) + lat
-        intersection_lng = 180*x / (R*Math.cos( intersection_lat )) + lng
-
-        return [intersection_lat, intersection_lng]
-      end
+        intersection_lng = x / ( Math.cos(intersection_lat*Rad)*R) * (180/Math::PI) + lng
+        
+        [intersection_lat, intersection_lng] if (@a[1]..@b[1]).include?(intersection_lng) || (@b[1]..@a[1]).include?(intersection_lng)
+        
+      end.compact.sort_by{|x,y| y }.first
             
     elsif delta < 0
-      p "< 0"
+      nil
     else
       p "==0"
     end
-    
   end
-
 end
